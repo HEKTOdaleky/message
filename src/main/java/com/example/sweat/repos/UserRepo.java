ad194d80b9fa4eeb58ca326ddb95a01a48d5ba0e
Maksim Gachevskiy <maksim.gachevskiy@zensoft.io>
2019-07-08 22:29:21 +0600

    Added login page

---
+++ src/main/java/com/example/sweat/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.sweat.repos;
+
+import com.example.sweat.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
